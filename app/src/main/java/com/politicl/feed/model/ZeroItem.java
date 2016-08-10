af74bf173c68764505be029cd29885eb91bb2a7e
trujunzhang <trujunzhang@gmail.com>
2016-08-10 21:14:47 +0800

    .politicl

--- app/src/main/java/com/politicl/feed/model/ZeroItem.java
+++ app/src/main/java/com/politicl/feed/model/ZeroItem.java
@@ -2,8 +2,13 @@ package com.politicl.feed.model;
 
 import android.support.annotation.NonNull;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ZeroItem {
 
-    @SuppressWarnings("unused,NullableProblems") @NonNull private int num;
+    @SuppressWarnings("unused,NullableProblems")
+    @SerializedName("0")
+    @NonNull
+    private int num;
 
 }
