a221a9b2e36fd9a4c886c124580381a957820724
trujunzhang <trujunzhang@gmail.com>
2016-08-14 20:00:12 +0800

    politicl.

--- app/src/main/java/com/politicl/feed/model/PostCard.java
+++ app/src/main/java/com/politicl/feed/model/PostCard.java
@@ -10,32 +10,72 @@ import com.politicl.util.StringUtil;
 
 import java.util.List;
 
-public class PostCard{
+public class PostCard {
 
-    @SuppressWarnings("unused,NullableProblems") @NonNull private int id;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String type;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String slug;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String url;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String status;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String title;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String title_plain;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String content;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String excerpt;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String date;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String modified;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private int comment_count;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String comment_status;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String thumbnail;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private String thumbnail_size;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private int id;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String type;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String slug;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String url;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String status;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String title;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String title_plain;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String content;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String excerpt;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String date;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String modified;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private int comment_count;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String comment_status;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String thumbnail;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private String thumbnail_size;
 
-    @SuppressWarnings("unused,NullableProblems") @NonNull private AuthorItem author;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private AuthorItem author;
 
-    @SuppressWarnings("unused,NullableProblems") @NonNull private ThumbnailItem thumbnail_images;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private ThumbnailItem thumbnail_images;
 
-    @SuppressWarnings("unused,NullableProblems") @NonNull private List<TagItem> tags;
-    @SuppressWarnings("unused,NullableProblems") @NonNull private List<PostCategoryItem> categories;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private List<TagItem> tags;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private List<PostCategoryItem> categories;
 
-    @SuppressWarnings("unused,NullableProblems") @NonNull private CustomFieldsItem custom_fields;
+    @SuppressWarnings("unused,NullableProblems")
+    @NonNull
+    private CustomFieldsItem custom_fields;
 
     @NonNull
     public String title() {
@@ -62,6 +102,7 @@ public class PostCard{
     public String author() {
         return author.author();
     }
+
     @NonNull
     public String customSourceUrl() {
         return custom_fields.customSourceUrl();
@@ -74,13 +115,16 @@ public class PostCard{
 
     @Nullable
     public String thumbnail() {
-        return thumbnail_images.thumbnail();
+        if (thumbnail_images != null) {
+            return thumbnail_images.thumbnail();
+        }
+        return null;
     }
 
 
     public Card getFeaturedArticleCard(Site site) {
 //        CardPageItem page = new CardPageItem();
 //        UtcDate date = new UtcDate(10);
-        return new FeaturedArticleCard(this,date,site);
+        return new FeaturedArticleCard(this, date, site);
     }
 }
